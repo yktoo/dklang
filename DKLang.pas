a920144a044eb20757c1e7b6b015a606fc9cf909
Dmitry Kann <yktooo@gmail.com>
2004-09-25 18:49:36 +0000

    Заготовка для универсального perl-модуля составления доки по API

--- DKLang.pas
+++ DKLang.pas
@@ -1,20 +1,22 @@
-//**********************************************************************************************************************
-//  $Id: DKLang.pas,v 1.18 2004-09-25 07:29:40 dale Exp $
-//----------------------------------------------------------------------------------------------------------------------
-//  DKLang Localization Package
-//  Copyright 2002-2004 DK Software, http://www.dk-soft.org/
-//**********************************************************************************************************************
-//
-// The contents of this package are subject to the Mozilla Public License
-// Version 1.1 (the "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
-//
-// Software distributed under the License is distributed on an "AS IS" basis,
-// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
-// specific language governing rights and limitations under the License.
-//
-// The initial developer of the original code is Dmitry Kann, http://www.dk-soft.org/
-//
+///**********************************************************************************************************************
+///  $Id: DKLang.pas,v 1.19 2004-09-25 18:49:36 dale Exp $
+///----------------------------------------------------------------------------------------------------------------------
+///  DKLang Localization Package
+///  Copyright 2002-2004 DK Software, http://www.dk-soft.org/
+///**********************************************************************************************************************
+///
+/// The contents of this package are subject to the Mozilla Public License
+/// Version 1.1 (the "License"); you may not use this file except in compliance
+/// with the License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+///
+/// Software distributed under the License is distributed on an "AS IS" basis,
+/// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+/// specific language governing rights and limitations under the License.
+///
+/// The initial developer of the original code is Dmitry Kann, http://www.dk-soft.org/
+///
+// The main unit (and the only required runtime unit) of the package. Contains all the
+// basic class and component declarations
 unit DKLang;
 
 interface
