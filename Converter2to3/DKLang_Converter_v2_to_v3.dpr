1ce7c6ccb1cc0d58ce7394c57edc4fd4e8e260cb
Dmitry Kann <yktooo@gmail.com>
2006-08-10 16:31:55 +0000

    Created converter for v2 to v3 constant resource conversion

---
+++ Converter2to3/DKLang_Converter_v2_to_v3.dpr
@@ -0,0 +1,13 @@
+program DKLang_Converter_v2_to_v3;
+
+uses
+  Forms,
+  Main in 'Main.pas' {fMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfMain, fMain);
+  Application.Run;
+end.
