c78c26819b8eaf5be20b2f091dbca5c73b065b71
Dmitry Kann <yktooo@gmail.com>
2006-08-11 12:17:06 +0000

    Packages updated, added BDS 2006 packages

--- _cleanup_.bat
+++ _cleanup_.bat
@@ -1,4 +1,5 @@
 cd "C:\Delphi\CVS projects\dale\DKLang" 
 del /s /q *.~* *.dcu *.ddp *.log *.bak *.tmp *.identcache *.bdsproj.local Entries.Old Entries.Extra.Old 1>nul 2>nul
+del /s /q Packages\*.dsk Packages\*.dof Packages\*.cfg 1>nul 2>nul
 rmdir /s /q Packages\__history 1>nul 2>nul
 rmdir /s /q Packages\ModelSupport 1>nul 2>nul
