bf32f5a9abb7ab612fce956fbd6d4ddea3ad3545
Dmitry Kann <yktooo@gmail.com>
2006-08-11 09:56:24 +0000

    Обновлены пакеты для Delphi 2005

--- _cleanup_.bat
+++ _cleanup_.bat
@@ -1,2 +1,4 @@
 cd "C:\Delphi\CVS projects\dale\DKLang" 
-del /s /q *.~* *.dcu *.ddp *.log *.bak *.tmp Entries.Old Entries.Extra.Old 1>nul 2>nul
+del /s /q *.~* *.dcu *.ddp *.log *.bak *.tmp *.identcache *.bdsproj.local Entries.Old Entries.Extra.Old 1>nul 2>nul
+rmdir /s /q Packages\__history 1>nul 2>nul
+rmdir /s /q Packages\ModelSupport 1>nul 2>nul
