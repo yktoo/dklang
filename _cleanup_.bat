5a465ffe331fc6c119e89ecde4cfa067cc644979
Dmitry Kann <yktooo@gmail.com>
2008-12-25 18:54:55 +0000

    Added keywords property, added file header.

--- _cleanup_.bat
+++ _cleanup_.bat
@@ -1,3 +1,10 @@
+@echo off
+rem ********************************************************************************************************************
+rem $Id$
+rem --------------------------------------------------------------------------------------------------------------------
+rem DKLang Localization Package
+rem Copyright 2002-2009 DK Software, http://www.dk-soft.org/
+rem ********************************************************************************************************************
 cd "C:\Delphi\CVS projects\dale\DKLang" 
 del /s /q *.~* *.dcu *.ddp *.log *.bak *.tmp *.identcache *.bdsproj.local Entries.Old Entries.Extra.Old 1>nul 2>nul
 del /s /q Packages\*.dsk Packages\*.dof Packages\*.cfg 1>nul 2>nul
