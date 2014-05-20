8ff7bcc578477550f83cada8325e350170ba50ae
bjmillerpa <bjmiller@gmail.com>
2014-05-20 18:07:07 -0400

    Updated packages for XE6

--- Packages/build_all.bat
+++ Packages/build_all.bat
@@ -1,7 +1,10 @@
-rem change parameter to reflect the version of Delphi you are installing to
-rem XE5 = 12.0
+rem change parameters to reflect the version of Delphi you are installing to
 
-set BDSVersion=12.0
+rem XE5 = 12.0, C:\Users\Public\Documents\RAD Studio\%BDSVersion%\Bpl
+rem XE6 = 14.0, C:\Users\Public\Documents\Embarcadero\Studio\%BDSVersion%\Bpl
+
+set BDSVersion=14.0
+set BDSBplPath=C:\Users\Public\Documents\Embarcadero\Studio\%BDSVersion%\Bpl
 
 rem prepares for msbuild
 call rsvars.bat
@@ -18,7 +21,7 @@ rem build design time package for only win32
 msbuild dcldklang.dproj /t:Rebuild /p:Config=Debug;Platform=Win32
 
 rem install design time package
-reg add "HKEY_CURRENT_USER\Software\Embarcadero\BDS\%BDSVersion%\Known Packages" /v "C:\Users\Public\Documents\RAD Studio\%BDSVersion%\Bpl\dcldklang.bpl" /d "DKLang Localization Package - IDE Tools" /f
+reg add "HKEY_CURRENT_USER\Software\Embarcadero\BDS\%BDSVersion%\Known Packages" /v "%BDSBplPath%\dcldklang.bpl" /d "DKLang Localization Package - IDE Tools" /f
 
 rem cleanup
 del *.local
